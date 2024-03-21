a176c80ab6f61b4b6cf587d45ef85becefc5d562
CecilieHorshauge <cecihors@gmail.com>
2024-03-21 09:40:41 +0100

    Pseudokode indsat

--- sketch.js
+++ sketch.js
@@ -1,7 +1,34 @@
 function setup() {
   createCanvas(400, 400);
+  // restartknap
+  // Lav knap btn1
+  // Lav knap btn2
+  // Lav knap btn3
+  // Lav knap btn4
 }
 
 function draw() {
   background(220);
 }
+// Pseudokode
+
+randomArray defineres
+playerArray initialiseres
+
+// Knapper
+
+
+// Spiller input
+Når btn1 trykkes på "pushes" værdien 1 i playerArray
+Når btn2 trykkes på "pushes" værdien 2 i playerArray
+Når btn3 trykkes på "pushes" værdien 3 i playerArray
+Når btn4 trykkes på "pushes" værdien 4 i playerArray
+
+// Sammenligning
+
+funktion Comparison
+for i < længden af randomArray
+  hvis RandomArray[i] = PlayerArray[i]
+    ellers udskriv "You failed" i konsollen
+  hvis i = længden af randomArray - 1
+  så udskriv "Congratulations"
