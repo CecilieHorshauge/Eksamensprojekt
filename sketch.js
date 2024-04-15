99ddbac6b6d5ac02517e8897daabd33ce4fb02f9
CecilieHorshauge <cecihors@gmail.com>
2024-04-15 09:49:57 +0200

    animation finally works

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
 let btn1, btn2, btn3, btn4;
-
 let Buttons = [];
-let farver = ["rgb(255, 182, 193)","rgb(107, 70, 227)", "rgb(145, 196, 108)", "rgb(227, 220, 93)" ];
+let farver = ["rgb(255, 182, 193)", "rgb(107, 70, 227)", "rgb(145, 196, 108)", "rgb(227, 220, 93)"];
 let CurrentColor = 0;
 let btnStart, btnSubmit;
+let playerArray = [];
+let randomArray = [];
 function setup() {
   createCanvas(400, 400);
   // restartknap
@@ -20,39 +21,33 @@ function setup() {
   btn4 = createButton("btn4")
   // Spiller input
   // btnStart.mousePressed(() => DisplaySequence())
+  frameRate(1)
   btnSubmit.mousePressed(() => Comparison())
   btn1.mousePressed(() => playerArray.push(1))
   btn2.mousePressed(() => playerArray.push(2))
   btn3.mousePressed(() => playerArray.push(3))
   btn4.mousePressed(() => playerArray.push(4))
-}
-
-   // Opretter fire knapper og gemmer dem i knappe-arrayet
-   for (let i = 0; i < 4; i++) {
+  // Opretter fire knapper og gemmer dem i knappe-arrayet
+  for (let i = 0; i < 4; i++) {
     let Btn1 = createButton("");
     Btn1.position(20, i * 50 + 20);
-    Btn1.size(25,25)
+    Btn1.size(25, 25)
     Btn1.style("background-color", "white");
-    
+
     Buttons.push(Btn1);
   }
+  
+}
+randomArray = [1, 2, 3, 4];
+console.log(randomArray)
 
 
-frameRate(1)
-
-// Sætter frameRate til 0 og stopper animationen 
-btn4.mousePressed(() => frameRate(0))
 
 function draw() {
   background(220);
-  if (randomArray.length === playerArray.length) {
-    Comparison()
-  }
+  ChangeColor(Buttons[CurrentColor % Buttons.length]);
+  CurrentColor++;
 }
-
-let randomArray = [1, 2, 3, 4];
-console.log(randomArray)
-let playerArray = [];
 
 // Sammenligning
 
@@ -62,26 +57,9 @@ function Comparison() {
     if (randomArray[i] !== playerArray[i]) { indentical = false }
   }
   if (indentical) { console.log("Congratulations") }
-    else {console.log("You failed... Congrats")}
-  playerArray=[]
-}
-
-  
-
-let randomArray = [1, 2, 3, 4];
-console.log(randomArray)
-let playerArray = [];
-
-// Sammenligning
-
-function Comparison() {
-  let indentical = true;
-  for (let i = 0; i < randomArray.length; i++) {
-    if (randomArray[i] !== playerArray[i]) { indentical = false }
-  }
-  if (indentical) { console.log("Congratulations") }
-    else {console.log("You failed... Congrats")}
-  playerArray=[]
+  else { console.log("You failed... Congrats") }
+  frameRate(0)
+  playerArray = []
 }
 
 //funktion til at skifte farve
