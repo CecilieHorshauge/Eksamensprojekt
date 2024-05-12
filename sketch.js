bd6eea74dae1fca15903a830cdbd470fc00cb016
CecilieHorshauge <cecihors@gmail.com>
2024-05-12 10:38:17 +0200

    Sekvensudvidelse virker

--- sketch.js
+++ sketch.js
@@ -1,89 +1,112 @@
 let btn1, btn2, btn3, btn4;
 let Buttons = [];
-let farver = ["rgb(255, 182, 193)", "rgb(107, 70, 227)", "rgb(145, 196, 108)", "rgb(227, 220, 93)"];
-let CurrentColor = 0;
+let message = "";
+
+//Farverne defineres i et array
+let farver = [
+  "rgb(255, 182, 193)",
+  "rgb(107, 70, 227)",
+  "rgb(145, 196, 108)",
+  "rgb(227, 220, 93)",
+];
 let btnStart, btnSubmit;
 let playerArray = [];
 let randomArray = [];
+
 function setup() {
-  createCanvas(400, 400);
-  // restartknap
-  // Lav start- og submit-knap
-  btnStart = createButton("Start")
-  btnSubmit = createButton("Submit")
-  // Lav knap btn1
-  btn1 = createButton("btn1")
-  btn1.position(50,20);
-  btn1.size(50,25)
-  // Lav knap btn2
-  btn2 = createButton("btn2")
-  btn2.position(50,70);
-  btn2.size(50,25)
-  // Lav knap btn3
-  btn3 = createButton("btn3")
-  btn3.position(50,120);
-  btn3.size(50,25)
-  // Lav knap btn4
-  btn4 = createButton("btn4")
-  btn4.position(50,170);
-  btn4.size(50,25)
+  createCanvas(400, 215);
+  background(220)
+  btnStart = createButton("Start");
+  btnSubmit = createButton("Submit");
 
-  // Spiller input
-  // btnStart.mousePressed(() => DisplaySequence())
-  frameRate(1)
-  btnSubmit.mousePressed(() => Comparison())
-  btn1.mousePressed(() => playerArray.push(1))
-  btn2.mousePressed(() => playerArray.push(2))
-  btn3.mousePressed(() => playerArray.push(3))
-  btn4.mousePressed(() => playerArray.push(4))
-  // Opretter fire knapper og gemmer dem i knappe-arrayet
+ // Der laves en knap til farven pink
+  btn1 = createButton("Pink");
+  btn1.position(50, 20);
+  btn1.size(50, 25);
+
+// Der laves en knap til farven blå
+  btn2 = createButton("Blue");
+  btn2.position(50, 70);
+  btn2.size(50, 25);
+
+// Der laves en knap til farven grøn
+  btn3 = createButton("Green");
+  btn3.position(50, 120);
+  btn3.size(50, 25);
+
+// Der laves en knap til farven gul
+  btn4 = createButton("Yellow");
+  btn4.position(50, 170);
+  btn4.size(50, 25);
+
+  // Der oprettes fire knapper og de gemems i knappe-arrayet
   for (let i = 0; i < 4; i++) {
-    let Btn1 = createButton("");
-    Btn1.position(20, i * 50 + 20);
-    Btn1.size(25, 25)
-    Btn1.style("background-color", "white");
-
-    Buttons.push(Btn1);
+    let Btn = createButton("");
+    Btn.position(20, i * 50 + 20);
+    Btn.size(25, 25);
+    Buttons.push(Btn);
   }
-  randomArray = [random([1,2,3,4])];
-  console.log(randomArray)
+
+  btn1.mousePressed(() => playerArray.push(0)); // Gemmer indekset for farven
+  btn2.mousePressed(() => playerArray.push(1));
+  btn3.mousePressed(() => playerArray.push(2));
+  btn4.mousePressed(() => playerArray.push(3));
+
+  // Startknap
+  btnStart.mousePressed(() => {
+    randomArray.push(Math.floor(Math.random() * 4)); // Tilføj et nyt tilfældigt index
+    displaySequence(); // Vis sekvens
+  });
+
+  btnSubmit.mousePressed(Comparison);
+  textSize(20)
+}
+function draw(){
+  text(message, 160,110)
 }
 
-
-
-
-function draw() {
-  background(220);
-  ChangeColor(Buttons[CurrentColor % Buttons.length]);
-  CurrentColor++;
+function displaySequence() {
+  let i = 0;
+  let interval = setInterval(() => {
+    if (i >= randomArray.length) {
+      clearInterval(interval);
+      return;
+    }
+    let colorIndex = randomArray[i];
+    Buttons[colorIndex].style("background-color", farver[colorIndex]); // Vis farve
+    setTimeout(
+      () => Buttons[colorIndex].style("background-color", "white"),
+      500
+    ); // Nulstil farve efter 0.5 sekund
+    i++;
+  }, 1000);
 }
-
-// Sammenligning
 
 function Comparison() {
-  let indentical = true;
-  for (let i = 0; i < randomArray.length; i++) {
-    if (randomArray[i] !== playerArray[i]) { indentical = false }
+  let identical = true;
+  if (playerArray.length !== randomArray.length) identical = false; // Tjek længden først
+  for (let i = 0; i < randomArray.length && identical; i++) {
+    if (randomArray[i] !== playerArray[i]) {
+      identical = false;
+    }
   }
-  if (indentical) {
-    console.log("Congratulations")
-    randomArray.push(random([1,2,3,4]))
-    console.log(randomArray)
+  playerArray = []; // Ryd spillerens indtastninger
+  if (identical) {
+    HideMessage()
+    message = "Congratulations";
+    setTimeout(HideMessage,1500)
+    randomArray.push(Math.floor(Math.random() * 4)); // Tilføj et nyt tilfældigt index
+    setTimeout(displaySequence, 1000); // Vent lidt og vis ny sekvens
+  } else {
+    randomArray = [];
+    HideMessage()
+    message = "You failed... Try again";
+    setTimeout(HideMessage,1500)
   }
-  else { 
-    console.log("You failed... Congrats")
-    frameRate(0)
-  }
-  playerArray = []
 }
 
-//funktion til at skifte farve
-function ChangeColor(Btn1) {
-  let originalFarve = Btn1.style("background-color"); // Gem den originale farve
-  let nyFarve = farver[CurrentColor % farver.length]; // Vælg en ny farve baseret på nuværende indeks
-  Btn1.style("background-color", nyFarve); // Sæt ny farve
-
-  setTimeout(() => {
-    Btn1.style("background-color", originalFarve); // Sæt tilbage til original efter 1 sekund
-  }, 1000);
+function HideMessage(){
+  clear()
+  background(220)
+  message = ""
 }
